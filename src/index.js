86201bb9f21f8f7a78a76c22ea0988710fa8134b
Anjali Asha <66244077+anjaliasha123@users.noreply.github.com>
2021-10-05 17:31:47 +0530

    comment1-blog created the component for first comment section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            ABC
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 pm</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
