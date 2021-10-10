45e26b0683956bf57c6fee345cc9800c9e1e7d66
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-10 18:43:37 +0530

    props using props for components

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail 
+      author="Sam"
+      dateTime="Today 6:15pm"
+      comment="Its a wonderfull pic!"/>
+      <CommentDetail 
+      author="Alex"
+      dateTime="Today 7:05pm"
+      comment="Keep Rocking"/>
+      <CommentDetail 
+      author="Jonas"
+      dateTime="Today 10:15pm"
+      comment="Kudosss"/>
     </div>
   );
 };
