e30ad082e4add3ded8cf50f3db2c7cce7f16df8a
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-11 16:29:06 +0530

    approval-card -creation creating the approval card. More changes to come

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-      author="Sam"
-      dateTime="Today 6:15pm"
-      comment="Its a wonderfull pic!"/>
-      <CommentDetail 
-      author="Alex"
-      dateTime="Today 7:05pm"
-      comment="Keep Rocking"/>
-      <CommentDetail 
-      author="Jonas"
-      dateTime="Today 10:15pm"
-      comment="Kudosss"/>
+      <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        dateTime="Today 6:15pm"
+        comment="Its a wonderfull pic!"
+      />
+      <CommentDetail
+        author="Alex"
+        dateTime="Today 7:05pm"
+        comment="Keep Rocking"
+      />
+      <CommentDetail
+        author="Jonas"
+        dateTime="Today 10:15pm"
+        comment="Kudosss"
+      />
     </div>
   );
 };
